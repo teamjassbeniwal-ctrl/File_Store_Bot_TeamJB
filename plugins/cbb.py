83d4059cd3099dd6339c6572fc8fc8d74cb00169
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-01-24 21:19:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,28 +1,33 @@
-#(©)Codexbotz
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+
     if data == "about":
+        await query.answer()
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='@TeamJB_bot'>Click here</a>\n○ Channel : @teamjb1\n○ Support Group : @botsupdatesgroup</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
-                ]
+            text=(
+                f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n"
+                f"○ Language : <code>Python3</code>\n"
+                f"○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n"
+                f"○ Source Code : <a href='@TeamJB_bot'>Click here</a>\n"
+                f"○ Channel : @teamjb1\n"
+                f"○ Support Group : @botsupdatesgroup</b>"
+            ),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔒 Close", callback_data="close")]]
             )
         )
+
     elif data == "close":
+        await query.answer()
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
-        except:
+        except Exception:
             pass
