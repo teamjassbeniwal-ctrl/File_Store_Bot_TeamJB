7de79860fbf7ce8084d1da45a243b80167bf6633
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-01-24 20:45:39 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -1,1 +1,55 @@
-#
+import time
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
+
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+database = dbclient[DB_NAME]
+
+user_data = database['users']
+
+default_verify = {
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': "",
+    'first_start': 0
+}
+
+def new_user(user_id):
+    return {
+        '_id': user_id,
+        'verify_status': {
+            'is_verified': False,
+            'verified_time': 0,
+            'verify_token': "",
+            'link': "",                     # ✅ COMMA ADDED
+            'first_start': int(time.time()) # 🕒 3 HOURS TIMER START
+        }
+    }
+
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user = new_user(user_id)
+    await user_data.insert_one(user)
+
+async def db_verify_status(user_id: int):
+    user = await user_data.find_one({'_id': user_id})
+    if user:
+        return user.get('verify_status', default_verify)
+    return default_verify
+
+async def db_update_verify_status(user_id: int, verify):
+    await user_data.update_one(
+        {'_id': user_id},
+        {'$set': {'verify_status': verify}}
+    )
+
+async def full_userbase():
+    user_docs = user_data.find()
+    return [doc['_id'] async for doc in user_docs]
+
+async def del_user(user_id: int):
+    await user_data.delete_one({'_id': user_id})
